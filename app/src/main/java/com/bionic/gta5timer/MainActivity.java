4d71d98de9dfe92e855bc58d3b7cf054df587439
Bionic-Leha <bionic.mods@gmail.com>
2015-01-19 02:37:20 +0200

    Убрал лишнее из кода

--- app/src/main/java/com/bionic/gta5timer/MainActivity.java
+++ app/src/main/java/com/bionic/gta5timer/MainActivity.java
@@ -26,55 +26,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         Global.gta5date = 83;
-
-        Calendar now = Calendar.getInstance();
-        Global.currentday = now.get(Calendar.DAY_OF_YEAR);
-
-        Global.second_now = now.get(Calendar.SECOND);
-        Global.minute_now = now.get(Calendar.MINUTE);
-        Global.hour_now = now.get(Calendar.HOUR);
-        Global.day_now = now.get(Calendar.DAY_OF_MONTH);
-        Global.month_now = now.get(Calendar.MONTH);
-        Global.month_now = Global.month_now + 1;
-
-        Global.day_left = Global.day_gta - Global.day_now;
-        Global.month_left = Global.month_gta - Global.month_now;
-        Global.hour_left = Global.hour_gta - Global.hour_now;
-        Global.minute_left = Global.minute_gta - Global.minute_now;
-        Global.second_left = Global.second_gta - Global.second_now;
-
-        if (Global.minute_left < 0){
-            Global.hour_left = Global.hour_left - 1;
-            Global.minute_left = Global.minute_left + 60;
-        }
-
-        if (Global.second_left < 0){
-            Global.minute_left = Global.minute_left - 1;
-            Global.second_left = Global.second_left + 60;
-        }
-
-        /*
-        if (sTime == 0L) {
-            sTime = SystemClock.uptimeMillis();
-            mHandler.removeCallbacks(TimeUpdater);
-            // Добавляем Runnable-объект TimeUpdater в очередь
-            // сообщений, объект должен быть запущен после задержки в 100 мс
-            mHandler.postDelayed(TimeUpdater, 100);
-        }
-        */
-
-        tvMain = (TextView) findViewById(R.id.tvMain);
-        tvMain.setText("GTA 5 to left: " + "« " + Global.month_left + " month, " + Global.day_left
-                + " days, " + Global.hour_left + " hours, " + Global.minute_left + " minutes, " + Global.second_left + " seconds " + "»");
-
+        
         mHandler.removeCallbacks(TimeUpdater);
 
-
     }
-
-
-
-
 
     // Описание Runnable-объекта
     private Runnable TimeUpdater = new Runnable() {
@@ -95,12 +50,12 @@ public class MainActivity extends ActionBarActivity {
             Global.minute_left = Global.minute_gta - Global.minute_now;
             Global.second_left = Global.second_gta - Global.second_now;
 
-            if (Global.minute_left <= 0){
+            if (Global.minute_left <= 0) {
                 Global.hour_left = Global.hour_left - 1;
                 Global.minute_left = Global.minute_left + 60;
             }
 
-            if (Global.second_left < 0){
+            if (Global.second_left < 0) {
                 Global.minute_left = Global.minute_left - 1;
                 Global.second_left = Global.second_left + 60;
             }
