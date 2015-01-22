9c104038bfae96809b7d7c3184d15f159c980f8a
Bionic-Leha <bionic.mods@gmail.com>
2015-01-22 03:04:31 +0200

    Создан активити инфы о игра�

--- app/src/main/java/com/bionic/gta5timer/activities/GameActivity.java
+++ app/src/main/java/com/bionic/gta5timer/activities/GameActivity.java
@@ -1,6 +1,8 @@
 package com.bionic.gta5timer.activities;
 
 import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -13,29 +15,11 @@ import com.bionic.gta5timer.R;
  */
 public class GameActivity extends ActionBarActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // previously invisible view
-        View myView = findViewById(R.id.card_view);
-
-        // get the center for the clipping circle
-        int cx = (myView.getLeft() + myView.getRight()) / 2;
-        int cy = (myView.getTop() + myView.getBottom()) / 2;
-
-        // get the final radius for the clipping circle
-        int finalRadius = Math.max(myView.getWidth(), myView.getHeight());
-
-        // create the animator for this view (the start radius is zero)
-        Animator anim =
-                ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
-
-        // make the view visible and start the animation
-        myView.setVisibility(View.VISIBLE);
-        anim.start();
 
     }
 
