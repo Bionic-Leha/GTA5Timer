3510165059ffcef99b2e581fb8244e50c68d7dd1
Bionic-Leha <bionic.mods@gmail.com>
2015-01-30 21:25:18 +0200

    Revert "GameActivity codding from wally"
    This reverts commit 12f365a2dc7dffce52d4ec0c6af04cdb902e7de6.

--- app/src/main/java/com/bionic/gamestimer/activities/GameActivity.java
+++ app/src/main/java/com/bionic/gamestimer/activities/GameActivity.java
@@ -1,203 +1,21 @@
 package com.bionic.gamestimer.activities;
 
-import android.animation.ValueAnimator;
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.bionic.gamestimer.R;
-import com.bionic.gamestimer.base.BaseActivity;
-
-import uk.co.senab.photoview.PhotoView;
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 /**
  * Created by Leha on 22.01.2015.
  */
-public class GameActivity extends BaseActivity implements Handler.Callback {
-
-    private Handler uiHandler;
-    private Handler backgroundHandler;
-
-    private ObservableScrollView scrollView;
-    private PhotoView photoView;
-    private PhotoViewAttacher photoViewAttacher;
-    private ImageButton buttonFullscreen;
-    private ProgressBar loader;
-    private Uri pageUri;
-    private ShareActionProvider shareActionProvider;
-    private TextView textViewUploader;
-    private TextView textViewUploadDate;
-    private TextView textViewSource;
-    private TextView textViewResolution;
-    private TextView textViewCategory;
-    private TextView textViewRating;
-    private Button buttonSetAs;
-    private Button buttonSave;
-    private ImagePage imagePage;
-    private ViewGroup imageHolder;
-    private ViewGroup photoLayoutHolder;
-
-    private int currentHandlerCode;
-    private Palette palette;
-    private ViewGroup toolbar;
-    private ViewGroup specsLayout;
-    private Size imageSize;
-
-    private boolean isInFullscreen = false;
-    private View detailsViewGroup;
+public class GameActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_details);
-
-        setToolbar((Toolbar) findViewById(R.id.toolbar));
-
-        if (getToolbar() != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                getWindow().setStatusBarColor(Color.TRANSPARENT);
-                getWindow().getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                getToolbar().setPadding(0, getStatusBarHeight(), 0, 0);
-            }
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("");
-        }
-
-        final Intent intent = getIntent();
-        String action = intent.getAction();
+        setContentView(R.layout.activity_game);
 
 
     }
 
-
-    @Override
-    public boolean handleMessage(Message msg) {
-        return false;
-    }
-
-    public int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
-
-    private void setupPaddings(final Size size, boolean animate) {
-
-        int animationDuration = animate ? 300 : 0;
-
-        photoView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        photoViewAttacher.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-        final int sidePadding = getResources().getDimensionPixelSize(R.dimen.activity_details_scrollview_side_padding);
-        int fabPadding = getResources().getDimensionPixelSize(R.dimen.fab_padding_positive);
-
-        int minimumAllowedHeight = fabPadding;
-
-        if (size.getHeight() < minimumAllowedHeight) {
-            size.setHeight(size.getHeight());
-            ValueAnimator valueAnimator = ValueAnimator.ofInt(photoLayoutHolder.getPaddingTop());
-            valueAnimator.setInterpolator(new EaseInOutBezierInterpolator());
-            valueAnimator.setDuration(animationDuration);
-            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                    photoLayoutHolder.setPadding(
-                            0,
-                            (Integer) valueAnimator.getAnimatedValue(),
-                            0,
-                            0
-                    );
-                }
-            });
-            valueAnimator.start();
-        } else {
-            photoLayoutHolder.setPadding(
-                    0,
-                    0,
-                    0,
-                    0
-            );
-        }
-
-        scrollView.setPadding(
-                0,
-                0,
-                0,
-                -fabPadding
-        );
-        specsLayout.setPadding(0, 0, 0, fabPadding);
-
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(detailsViewGroup.getPaddingTop(), size.getHeight());
-        valueAnimator.setInterpolator(new EaseInOutBezierInterpolator());
-        valueAnimator.setDuration(animationDuration);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                detailsViewGroup.setPadding(sidePadding,
-                        (Integer) valueAnimator.getAnimatedValue(),
-                        sidePadding,
-                        detailsViewGroup.getPaddingBottom());
-            }
-        });
-        valueAnimator.start();
-    }
-
-    private void setupViews() {
-        scrollView = (ObservableScrollView) findViewById(R.id.image_details_scrollview);
-        imageHolder = (ViewGroup) findViewById(R.id.image_details_imageview_holder);
-        photoView = (PhotoView) findViewById(R.id.image_details_imageview);
-        buttonFullscreen = (ImageButton) findViewById(R.id.image_details_button_fullscreen);
-        loader = (ProgressBar) findViewById(R.id.image_details_loader);
-        textViewUploader = (TextView) findViewById(R.id.image_details_uploader);
-        textViewUploadDate = (TextView) findViewById(R.id.image_details_upload_date);
-        textViewSource = (TextView) findViewById(R.id.image_details_source);
-        textViewResolution = (TextView) findViewById(R.id.image_details_resolution);
-        textViewCategory = (TextView) findViewById(R.id.image_details_category);
-        textViewRating = (TextView) findViewById(R.id.image_details_rating);
-        buttonSetAs = (Button) findViewById(R.id.toolbar_set_as);
-        buttonSave = (Button) findViewById(R.id.toolbar_save);
-        toolbar = (ViewGroup) findViewById(R.id.image_details_toolbar);
-        photoLayoutHolder = (ViewGroup) findViewById(R.id.image_details_photo_layout_holder);
-        specsLayout = (ViewGroup) findViewById(R.id.image_details_specs);
-        detailsViewGroup = findViewById(R.id.image_details_layout);
-
-        specsLayout.setAlpha(0.0f);
-
-        int sidePadding = getResources().getDimensionPixelSize(R.dimen.activity_details_scrollview_side_padding);
-        int fabPadding = getResources().getDimensionPixelSize(R.dimen.fab_padding_positive);
-        scrollView.setPadding(0, 0, 0, -fabPadding);
-        specsLayout.setPadding(0, 0, 0, fabPadding);
-        photoLayoutHolder.setPadding(0, 0, 0, 0);
-        detailsViewGroup.setPadding(
-                sidePadding,
-                detailsViewGroup.getPaddingTop(),
-                sidePadding,
-                detailsViewGroup.getPaddingBottom()
-        );
-
-        photoViewAttacher = new PhotoViewAttacher(photoView);
-        photoViewAttacher.setZoomable(false);
-        photoViewAttacher.setScaleType(ImageView.ScaleType.FIT_XY);
-    }
 }
