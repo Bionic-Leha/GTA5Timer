8947c69034e3284cf516ea8c802fc65b6eed26a3
Bionic-Leha <bionic.mods@gmail.com>
2015-02-01 01:42:32 +0200

    Edit listView

--- app/src/main/java/com/bionic/gamestimer/activities/BaseActivity.java
+++ app/src/main/java/com/bionic/gamestimer/activities/BaseActivity.java
@@ -25,16 +25,44 @@ import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.bionic.gamestimer.R;
 import com.bionic.gamestimer.adapters.SimpleHeaderRecyclerAdapter;
 import com.bionic.gamestimer.adapters.SimpleRecyclerAdapter;
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public abstract class BaseActivity extends ActionBarActivity {
     private static final int NUM_OF_ITEMS = 100;
+    private static final int NUM_IDS = 10;
     private static final int NUM_OF_ITEMS_FEW = 3;
+    private static final int NUM_OF_ITEMS_FEW_2 = 3;
+
+    public static String creator;
+    public static String publisher;
+    public static String platform;
+    public static String release_date;
+    public static String announce_date;
+    public static String genre;
+    public static String game_mode;
+    public static String site;
+    public static String icon;
+
+    private ArrayList<HashMap<String, Object>> infoList;
+    private static final String TITLE = "name game";
+    private static final String DESCRIPTION = "description of game";
+    private static final String CREATOR = "creator name";
+    private static final String PUBLISHER = "publisher studio";
+    private static final String PLATFORM = "game platform";
+    private static final String RELEASE_DATE = "date of release";
+    private static final String ANNOUNCE_DATE = "date of announce";
+    private static final String GENRE = "game genre";
+    private static final String GAME_MODE = "game mode";
+    private static final String SITE = "official site";
+    private static final String ICON = "screenshot of game";
 
     protected int getActionBarSize() {
         TypedValue typedValue = new TypedValue();
@@ -50,9 +78,16 @@ public abstract class BaseActivity extends ActionBarActivity {
         return findViewById(android.R.id.content).getHeight();
     }
 
+
     public static ArrayList<String> getDummyData() {
         return getDummyData(NUM_OF_ITEMS);
     }
+
+    public static ArrayList<HashMap<String, Object>> getListData(){
+        return getListData(NUM_IDS);
+    }
+
+    // NEW
 
     public static ArrayList<String> getDummyData(int num) {
         ArrayList<String> items = new ArrayList<String>();
@@ -62,16 +97,53 @@ public abstract class BaseActivity extends ActionBarActivity {
         return items;
     }
 
+    public static ArrayList<HashMap<String, Object>> getListData(int num){
+        ArrayList<HashMap<String, Object>> infoList = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> games;
+
+        games = new HashMap<String, Object>();
+        games.put(TITLE, "TEST"); // Название
+        games.put(DESCRIPTION, "TEST 2"); // Описание
+        games.put(PLATFORM, "TEST 3"); // Платформа
+        games.put(ICON, R.drawable.ic_gta); // Картинка
+        infoList.add(games);
+
+        return infoList;
+    }
+
+    // separator
+
     protected void setDummyData(ListView listView) {
         setDummyData(listView, NUM_OF_ITEMS);
     }
+
+    protected void setListData(ListView listView){
+        setListData(listView, NUM_IDS);
+    }
+
+    // separator
 
     protected void setDummyDataFew(ListView listView) {
         setDummyData(listView, NUM_OF_ITEMS_FEW);
     }
 
+    protected void setListDataFew(ListView listView) {
+        setDummyData(listView, NUM_OF_ITEMS_FEW);
+    }
+
+
+    // separator
+
     protected void setDummyData(ListView listView, int num) {
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getDummyData(num)));
+    }
+
+    public void setListData(ListView listView, int num) {
+        SimpleAdapter adapter = new SimpleAdapter(this, infoList,
+                R.layout.list_item, new String[]{TITLE, DESCRIPTION, PLATFORM, ICON},
+                new int[]{R.id.tvTitle, R.id.tvDesc, R.id.tvPlatforms, R.id.tvDate, R.id.img});
+
+        listView.setAdapter(adapter);
     }
 
     protected void setDummyDataWithHeader(ListView listView, int headerHeight) {
