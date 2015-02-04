8b98d66aad59bd8108265b50b73a5b2274743fe2
Bionic-Leha <bionic.mods@gmail.com>
2015-02-04 23:06:37 +0200

    New UI and dialogs

--- app/src/main/java/com/bionic/gamestimer/activities/MainActivity.java
+++ app/src/main/java/com/bionic/gamestimer/activities/MainActivity.java
@@ -1,11 +1,20 @@
 package com.bionic.gamestimer.activities;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,10 +22,12 @@ import com.bionic.gamestimer.Global;
 import com.bionic.gamestimer.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -28,12 +39,24 @@ public class MainActivity extends ActionBarActivity {
 
     int itemnum = 0;
 
+    AlertDialog.Builder ad;
+    Context context;
+
+    // идентификатор диалогового окна AlertDialog с кнопками
+    private final int IDD_THREE_BUTTONS = 0;
+    private final int FIRST_RUN = 1;
+
     private ArrayList<HashMap<String, Object>> gamesList;
     private static final String TITLE = "gamename"; // Название игры
     private static final String DESCRIPTION = "gamedesc"; // Описание игры
     private static final String PLATFORM = "gameplatf"; // Платформа
     private static final String DATE = "gamegate"; // Дата выхода
+    private static final String TIMER = "game_timer"; // Таймер выхода
     private static final String ICON = "icon";  // Скриншот игры
+
+    public static String info;
+
+    String link_2_wiki = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +65,14 @@ public class MainActivity extends ActionBarActivity {
         setTitle(getString(R.string.games_list));
 
         Global.gta5date = 83;
+
+        SharedPreferences saves = getSharedPreferences(Global.PREFS_SAVES, MODE_PRIVATE);
+
+        // Первый запуск!?
+        if (saves.getBoolean("first_launch_1", true)) {
+            showDialog(FIRST_RUN);
+            saves.edit().putBoolean("first_launch_1", false).apply();
+        }
 
         //mHandler.removeCallbacks(TimeUpdater);
 
@@ -71,6 +102,9 @@ public class MainActivity extends ActionBarActivity {
         String OUTDAY = getString(R.string.out_date);
         String comma = ", ";
 
+        String timer_gta = null;
+        String timer_witcher = null;
+
 
         ListView lvGames = (ListView) findViewById(R.id.lvGames);
 
@@ -83,6 +117,7 @@ public class MainActivity extends ActionBarActivity {
         games.put(DESCRIPTION, getString(R.string.gta5_desc_1)); // Описание
         games.put(PLATFORM, PC); // Платформа
         games.put(DATE, OUTDAY + ": " + getString(R.string.gta5_date_1)); // Дата выхода
+        //games.put(DATE, OUTDAY + ": " + timer_gta); // Дата выхода
         games.put(ICON, R.drawable.ic_gta); // Картинка
         gamesList.add(games);
 
@@ -91,8 +126,8 @@ public class MainActivity extends ActionBarActivity {
         games.put(DESCRIPTION, getString(R.string.witcher_desc2));
         games.put(PLATFORM, PC + comma + PS4 + comma + XONE);
         games.put(DATE, OUTDAY + ": " + getString(R.string.witcher_date2));
+        //games.put(DATE, OUTDAY + ": " + timer_witcher); // Дата выхода
         games.put(ICON, R.drawable.ic_witcher);
-        itemnum++;
         gamesList.add(games);
 
         SimpleAdapter adapter = new SimpleAdapter(this, gamesList,
@@ -100,12 +135,82 @@ public class MainActivity extends ActionBarActivity {
                 new int[]{R.id.tvTitle, R.id.tvDesc, R.id.tvPlatforms, R.id.tvDate, R.id.img});
 
         lvGames.setAdapter(adapter);
+
+        lvGames.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                switch (position) {
+                    case 0:
+                        link_2_wiki = "https://ru.wikipedia.org/wiki/Grand_Theft_Auto_V";
+                        Global.day_game = 24;
+                        Global.month_game = 5;
+                        mHandler.removeCallbacks(TimeUpdater);
+                        mHandler.postDelayed(TimeUpdater, 1000);
+                        showDialog(IDD_THREE_BUTTONS);
+                        break;
+                    case 1:
+                        Intent browserIntent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ru.wikipedia.org/wiki/The_Witcher_3:_Wild_Hunt"));
+                        startActivity(browserIntent1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
-    /*
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case FIRST_RUN:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.imp_info));
+                builder.setMessage(getString(R.string.first_run))
+                        .setCancelable(false)
+                        .setPositiveButton(getString(R.string.ok),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                return builder.create();
+
+            case IDD_THREE_BUTTONS:
+                AlertDialog.Builder builder_2 = new AlertDialog.Builder(this);
+                builder_2.setTitle(getString(R.string.detail_info));
+                builder_2.setMessage(MainActivity.info)
+                        .setCancelable(true)
+                        .setPositiveButton(getString(R.string.ok),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        mHandler.removeCallbacks(TimeUpdater);
+                                        dialog.cancel();
+                                    }
+                                })
+                        .setNeutralButton("Wiki",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link_2_wiki));
+                                        startActivity(browserIntent);
+                                        dialog.cancel();
+                                    }
+                                });
+
+                return builder_2.create();
+            default:
+                return null;
+        }
+    }
+
     // Описание Runnable-объекта
-    private Runnable TimeUpdater = new Runnable() {
+    public Runnable TimeUpdater = new Runnable() {
         public void run() {
+            Log.d("New", " ok");
             Calendar now = Calendar.getInstance();
             Global.currentday = now.get(Calendar.DAY_OF_YEAR);
 
@@ -117,11 +222,15 @@ public class MainActivity extends ActionBarActivity {
             Global.days_month = now.getActualMaximum(Calendar.DAY_OF_MONTH);
             Global.month_now = Global.month_now + 1;
 
-            Global.day_left = Global.day_gta - Global.day_now;
-            Global.month_left = Global.month_gta - Global.month_now;
-            Global.hour_left = Global.hour_gta - Global.hour_now;
-            Global.minute_left = Global.minute_gta - Global.minute_now;
-            Global.second_left = Global.second_gta - Global.second_now;
+            Global.hour_game = 12;
+            Global.minute_game = 0;
+            Global.second_game = 0;
+
+            Global.day_left = Global.day_game - Global.day_now;
+            Global.month_left = Global.month_game - Global.month_now;
+            Global.hour_left = Global.hour_game - Global.hour_now;
+            Global.minute_left = Global.minute_game - Global.minute_now;
+            Global.second_left = Global.second_game - Global.second_now;
 
             if (Global.day_left <= 0) {
                 Global.month_left -= 1;
@@ -138,20 +247,23 @@ public class MainActivity extends ActionBarActivity {
                 Global.second_left += 60;
             }
 
-            String info = getString(R.string.gtaleft_text) + ": « "
+            MainActivity.info = getString(R.string.gtaleft_text) + ": « "
                     + Global.month_left + " " + getString(R.string.month) + ", "
                     + Global.day_left + " " + getString(R.string.days) + ", "
                     + Global.hour_left + " " + getString(R.string.hours) + ", "
                     + Global.minute_left + " " + getString(R.string.minutes) + ", "
                     + Global.second_left + " " + getString(R.string.seconds) + "»";
 
-            tvMain = (TextView) findViewById(R.id.tvMain);
-            tvMain.setText(info);
+            //tvMain = (TextView) findViewById(R.id.tvMain);
+            //tvMain.setText(info);
 
             mHandler.postDelayed(this, 1000);
         }
     };
-    */
+
+    public void updGtaTimer(){
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -169,7 +281,12 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
-            startActivity(new Intent(MainActivity.this, GameInfoActivity.class));
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            return true;
+        }
+
+        if (id == R.id.action_info) {
+            showDialog(FIRST_RUN);
             return true;
         }
 
