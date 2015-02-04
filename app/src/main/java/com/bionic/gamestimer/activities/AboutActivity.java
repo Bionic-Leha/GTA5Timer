a3991be0605ccd9f97694e38fb88eda493269e8f
Bionic-Leha <bionic.mods@gmail.com>
2015-02-04 23:06:48 +0200

    Init

---
+++ app/src/main/java/com/bionic/gamestimer/activities/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.bionic.gamestimer.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.bionic.gamestimer.R;
+
+/**
+ * Created by Leha on 04.02.2015.
+ */
+public class AboutActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
