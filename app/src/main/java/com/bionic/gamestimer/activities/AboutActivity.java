ef632aa79e49291bc17897b2441f0b2d2e6dfd41
Bionic-Leha <bionic.mods@gmail.com>
2015-02-23 02:43:34 +0200

    Release 0.9.5 [2/2]

--- app/src/main/java/com/bionic/gamestimer/activities/AboutActivity.java
+++ app/src/main/java/com/bionic/gamestimer/activities/AboutActivity.java
@@ -1,20 +1,23 @@
 package com.bionic.gamestimer.activities;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
+import com.bionic.gamestimer.BuildConfig;
 import com.bionic.gamestimer.R;
 
 /**
  * Created by Leha on 04.02.2015.
  */
 public class AboutActivity extends ActionBarActivity {
+    TextView tvVersion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_about);
+        tvVersion = (TextView) findViewById(R.id.tvVersion);
+        tvVersion.setText(getString(R.string.version) + BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")");
     }
 }
